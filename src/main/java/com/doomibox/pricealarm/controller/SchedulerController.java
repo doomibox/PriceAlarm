ffa4cd1c38e181841ad2218b271ec22939117243
doomibox <kevinhu1997@gmail.com>
2020-04-16 00:53:22 -0700

    Enabled Quartz Scheduler

--- src/main/java/com/doomibox/pricealarm/controller/SchedulerController.java
+++ src/main/java/com/doomibox/pricealarm/controller/SchedulerController.java
@@ -1,5 +1,9 @@
 package com.doomibox.pricealarm.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,10 +11,16 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class SchedulerController {
 
+    @Autowired
+    private Scheduler scheduler;
+
     @RequestMapping(value = "/schedules", method = RequestMethod.GET)
-    public ResponseEntity<Object> getSchedules() {
+    public ResponseEntity<Object> getSchedules() throws SchedulerException {
+        log.info("GET schedules request");
+        log.info("Job Groups: {}", scheduler.getJobGroupNames().toString());
         return new ResponseEntity<>("Schedules", HttpStatus.ACCEPTED);
     }
 }
---
+++ src/main/java/com/doomibox/pricealarm/service/job/SendCrawlMessageJob.java
@@ -0,0 +1,14 @@
+package com.doomibox.pricealarm.service.job;
+
+import lombok.extern.slf4j.Slf4j;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+@Slf4j
+public class SendCrawlMessageJob implements Job {
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        log.info("Sending Crawl Message to SQS");
+    }
+}
