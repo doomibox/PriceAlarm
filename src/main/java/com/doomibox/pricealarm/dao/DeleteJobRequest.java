ef124a32aa253d3be13407461cd50b8213a10fb8
doomibox <kevinhu1997@gmail.com>
2020-04-17 19:51:24 -0700

    fix json parsing creator issue

--- src/main/java/com/doomibox/pricealarm/dao/DeleteJobRequest.java
+++ src/main/java/com/doomibox/pricealarm/dao/DeleteJobRequest.java
@@ -3,10 +3,12 @@ package com.doomibox.pricealarm.dao;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Data
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class DeleteJobRequest {
     private String jobKey;
 }
